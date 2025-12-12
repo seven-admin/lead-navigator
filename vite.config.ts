b72a9098cca7ba25f40b7d9cd4cbc17e8937ebbc
seven-admin <administrativo@sevengroup360.com.br>
2025-12-12 14:21:11 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
+    port: 8080
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ["leads.sevengroup360sys.com.br"]
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
 }));
